1ed05dfca62ffcf0fa726c09539df5f34ac32d80
tuturuga-bytes <78251645+tuturuga-bytes@users.noreply.github.com>
2023-03-27 15:39:02 +0700

    Update litttjs.js

--- litttjs.js
+++ litttjs.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 
 class RangeSlider extends LitElement {
   static get properties() {
